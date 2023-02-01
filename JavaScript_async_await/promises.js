85cf2807c190125706e419ccdce07aeb68e7501c
Andrei <andrei.alexe@outlook.com>
2023-02-01 23:30:45 +0200

    JS Async Await - Promise.all and Async / Await

--- JavaScript_async_await/promises.js
+++ JavaScript_async_await/promises.js
@@ -37,6 +37,38 @@ function createPost(post) {
   });
 }
 
-createPost({ title: "Post 3", body: "This is post 3" })
-    .then(getPosts)
-    .catch(err => console.log(err));
+// createPost({ title: "Post 3", body: "This is post 3" })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
+
+// Async / Await - the function must be labeled as async in order to use await (wait for an asynchronous process to complete)
+async function init() {
+  await createPost({ title: "Post 3", body: "This is post 3" });
+  getPosts();
+}
+init();
+
+// Async / Await with fetch
+async function fetchUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await res.json();
+  console.log(data);
+}
+fetchUsers();
+
+// Promises
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, "Goodbye")
+);
+
+// this promise fetches date from a fake API, then converts it to JSON to eliminate unwanted metadata that would otherwise show in the results of Promise.all.then
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
+
+// Promise.all takes in an array and then returns the results only after all of them have been resolved. In this case it takes 2seconds because that's how long the longest Promise takes to resolve.
+Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+  console.log(values)
+);
